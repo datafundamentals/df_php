0f37c20b78318d2d7f59230247c38d8d9346d0a4
jcarapet <jeffcarapetyan@gmail.com>
2015-01-26 11:30:37 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'df_php'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures df_php'
+long_description 'Installs/Configures df_php'
+version          '0.1.0'
